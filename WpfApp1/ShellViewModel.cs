f4c3ef00ab47c6c42e7da76cf17ddb75eb89df34
Jan Verley <Jan.Verley@agidens.com>
2022-12-25 22:53:30 +0100

    refac to bitarray

--- WpfApp1/ShellViewModel.cs
+++ WpfApp1/ShellViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Windows.Media;
 using System.Windows.Media.Media3D;
 using System.Windows.Threading;
 using HelixToolkit.Wpf;
@@ -14,12 +15,14 @@ namespace WpfApp1
     {
         private readonly DispatcherTimer timer;
 
-        private const int xSize = 100;
-        private const int ySize = 100;
-        private const int zSize = 100;
-        
-        private readonly bool[,,] cells = new bool[xSize, ySize, zSize];
+        private const int xSize = 20;
+        private const int ySize = 20;
+        private const int zSize = 20;
+
+        private byte[] cells2 = new byte[xSize * ySize * zSize];
+
         private int generation;
+        private int numberOfCells;
         private bool isRunning;
 
         public ShellViewModel()
@@ -30,27 +33,99 @@ namespace WpfApp1
 
             StartCommand = new DelegateCommand(Start, CanStart);
             StopCommand = new DelegateCommand(Stop, CanStop);
+            TickCommand = new DelegateCommand(() => OnTick(this, EventArgs.Empty));
+            SeedCommand = new DelegateCommand(Seed);
 
             Objects = new ObservableCollection<Visual3D>();
             Objects.Add(new DefaultLights());
-            Objects.Add(new GridLinesVisual3D { Length = 20, Width = 20, MinorDistance = 1, MajorDistance = 1, Thickness = 0.01 });
+            Objects.Add(new GridLinesVisual3D { Length = xSize, Width = ySize, MinorDistance = 1, MajorDistance = 1, Thickness = 0.01 });
 
-            cells[50, 50, 50] = true;
-            cells[49, 50, 50] = true;
-            cells[50, 51, 50] = true;
-            cells[47, 52, 50] = true;
-            cells[47, 53, 50] = true;
-            cells[48, 53, 50] = true;
-            
-            OnTick(this,EventArgs.Empty);
+            Seed();
         }
 
-        private static BoxVisual3D FromCoordinate(Coordinate c)
+        private static int Index(int x, int y, int z) => x + y * xSize + z * xSize * ySize;
+
+        private void Seed()
         {
-            return new BoxVisual3D { Center = new Point3D(c.X - (xSize / 2), c.Y - (ySize / 2), c.Z - (zSize / 2)) };
+            KillAllCells();
+
+            var rand = new Random();
+            var c = rand.Next(20, xSize * ySize * zSize / 10);
+            for (int i = 0; i < c; i++)
+            {
+                var x = rand.Next(0, xSize);
+                var y = rand.Next(0, ySize);
+                var z = rand.Next(0, zSize);
+                cells2[Index(x, y, z)] = 1;
+            }
+
+            UpdateNumberOfLiveCells();
+
+            OnTick(this, EventArgs.Empty);
+        }
+
+        private void KillAllCells()
+        {
+            for (int i = 0; i < xSize * ySize * zSize; i++)
+            {
+                cells2[i] = 0;
+            }
+        }
+
+        private void UpdateNumberOfLiveCells()
+        {
+            NumberOfCells = 0;
+            for (int ix = 0; ix < xSize; ix++)
+            {
+                for (int iy = 0; iy < ySize; iy++)
+                {
+                    for (int iz = 0; iz < zSize; iz++)
+                    {
+                        var index = Index(ix, iy, iz);
+                        if (IsAlive(index))
+                        {
+                            NumberOfCells++;
+                        }
+                    }
+                }
+            }
+        }
+
+
+        private BoxVisual3D FromCoordinate(int index)
+        {
+            int z = (index / (xSize * ySize));
+            int zRest = (index % (xSize * ySize));
+            
+            int y = zRest / xSize;
+            
+            var x = zRest % xSize;
+            return new BoxVisual3D
+            {
+                Center = new Point3D(x - xSize / 2, y - ySize / 2, z - zSize / 2), Fill =
+                    new SolidColorBrush(ColorByNeighbours(index))
+            };
+        }
+
+        private Color ColorByNeighbours(int index)
+        {
+            if (NumberOfLiveNeighbours(index) < 2)
+            {
+                return Colors.Green;
+            }
+            else if (NumberOfLiveNeighbours(index) > 3)
+            {
+                return Colors.DarkRed;
+            }
+            else
+            {
+                return Colors.Blue;
+            }
         }
 
         public DelegateCommand StopCommand { get; set; }
+        public DelegateCommand TickCommand { get; set; }
+        public DelegateCommand SeedCommand { get; set; }
 
 
         public bool IsRunning
@@ -70,10 +145,17 @@ namespace WpfApp1
             }
         }
 
+
         public int Generation
         {
             get => generation;
             private set => SetField(ref generation, value);
+        }
+
+        public int NumberOfCells
+        {
+            get => numberOfCells;
+            private set => SetField(ref numberOfCells, value);
         }
 
         public DelegateCommand StartCommand { get; set; }
@@ -84,103 +166,118 @@ namespace WpfApp1
 
         private void OnTick(object sender, EventArgs e)
         {
+            CalculateNextGen();
+
+            RebuildBlocksFromCells();
+        }
+
+        private void CalculateNextGen()
+        {
             Generation++;
+            var cellsNextGen2 = new byte[xSize * ySize * zSize];
+
+            for (int ix = 0; ix < xSize; ix++)
+            {
+                for (int iy = 0; iy < ySize; iy++)
+                {
+                    for (int iz = 0; iz < zSize; iz++)
+                    {
+                        var index = Index(ix, iy, iz);
+                        var numberOfLiveNeighbours = NumberOfLiveNeighbours(index);
+                        if (IsAlive(index))
+                        {
+                            if (numberOfLiveNeighbours < 2)
+                                cellsNextGen2[index] = 0;
+
+                            else if (numberOfLiveNeighbours == 2 || numberOfLiveNeighbours == 3)
+                                cellsNextGen2[index] = 1;
+
+                            else if (numberOfLiveNeighbours > 3)
+                                cellsNextGen2[index] = 0;
+                        }
+                        else if (numberOfLiveNeighbours == 3)
+                            cellsNextGen2[index] = 1;
+                    }
+                }
+            }
+
+            for (int ix = 0; ix < xSize; ix++)
+            {
+                for (int iy = 0; iy < ySize; iy++)
+                {
+                    for (int iz = 0; iz < zSize; iz++)
+                    {
+                        cells2[Index(ix, iy, iz)] = cellsNextGen2[Index(ix, iy, iz)];
+                    }
+                }
+            }
+        }
+
+        private void RebuildBlocksFromCells()
+        {
+            UpdateNumberOfLiveCells();
 
             foreach (var cell in Objects.OfType<BoxVisual3D>().ToList())
             {
                 Objects.Remove(cell);
             }
 
-            for (int ix = 0; ix < 100; ix++)
+            for (int ix = 0; ix < xSize; ix++)
             {
-                for (int iy = 0; iy < 100; iy++)
+                for (int iy = 0; iy < ySize; iy++)
                 {
-                    for (int iz = 0; iz < 100; iz++)
+                    for (int iz = 0; iz < zSize; iz++)
                     {
-                        var c = new Coordinate(ix, iy, iz);
-                        if (IsAlive(c))
+                        var index = Index(ix, iy, iz);
+                        if (IsAlive(index))
                         {
-                            if (NumberOfLiveNeighbours(c) < 2)
-                            {
-                                cells[c.X, c.Y, c.Z] = false;
-                            }
-
-                            if (NumberOfLiveNeighbours(c) == 2 || NumberOfLiveNeighbours(c) == 3)
-                            {
-                                cells[c.X, c.Y, c.Z] = true;
-                            }
-
-                            if (NumberOfLiveNeighbours(c) > 3)
-                            {
-                                cells[c.X, c.Y, c.Z] = false;
-                            }
-                        }
-                        else
-                        {
-                            if (NumberOfLiveNeighbours(c) == 3)
-                            {
-                                cells[c.X, c.Y, c.Z] = true;
-                            }
-                        }
-                    }
-                }
-            }
-
-            for (int ix = 0; ix < 100; ix++)
-            {
-                for (int iy = 0; iy < 100; iy++)
-                {
-                    for (int iz = 0; iz < 100; iz++)
-                    {
-                        var c = new Coordinate(ix, iy, iz);
-                        if (IsAlive(c))
-                        {
-                            Objects.Add(FromCoordinate(c));
+                            Objects.Add(FromCoordinate(index));
                         }
                     }
                 }
             }
         }
 
-        private bool IsAlive(Coordinate p)
+        private bool IsAlive(int index)
         {
-            if (p.X < 0 || p.X > xSize -1)
+            if (index >= 0)
             {
-                return false;
-            }
-            if (p.Y < 0 || p.Y > ySize -1)
-            {
-                return false;
-            }
-            
-            if (p.Z < 0 || p.Z > zSize-1)
-            {
-                return false;
+                if (index < xSize * ySize * zSize)
+                {
+                    if (cells2[index] == 1) 
+                        return true;
+                }
             }
 
-            return cells[p.X, p.Y, p.Z];
+            return false;
         }
 
-        private int NumberOfLiveNeighbours(Coordinate p)
+        private int NumberOfLiveNeighbours(int index)
         {
-            return NumberOfLiveNeighboursInThisPlane(p) +
-                   NumberOfLiveNeighboursInThisPlane(p.Before()) +
-                   (IsAlive(p.Before()) ? 1 : 0) +
-                   NumberOfLiveNeighboursInThisPlane(p.Behind()) +
-                   (IsAlive(p.Behind()) ? 1 : 0);
+            return NumberOfLiveNeighboursInThisPlane(index) +
+                   NumberOfLiveNeighboursInThisPlane(Before(index)) +
+                   (IsAlive(Before(index)) ? 1 : 0) +
+                   NumberOfLiveNeighboursInThisPlane(Behind(index)) +
+                   (IsAlive(Behind(index)) ? 1 : 0);
         }
-        
 
-        private int NumberOfLiveNeighboursInThisPlane(Coordinate p)
+        public static int Left(int index) => index -1;
+        public static int Right(int index) => index + 1;
+        public static int Up(int index) => index + xSize;
+        public static int Down(int index) => index - xSize;
+        public static int Before(int index) => index - (xSize * ySize);
+        public static int Behind(int index) => index + (xSize * ySize);
+
+        private int NumberOfLiveNeighboursInThisPlane(int index)
         {
-            return (IsAlive(p.Left()) ? 1 : 0) +
-                   (IsAlive(p.Right()) ? 1 : 0) +
-                   (IsAlive(p.Up()) ? 1 : 0) +
-                   (IsAlive(p.Down()) ? 1 : 0) +
-                   (IsAlive(p.Left().Up()) ? 1 : 0) +
-                   (IsAlive(p.Left().Down()) ? 1 : 0) +
-                   (IsAlive(p.Right().Up()) ? 1 : 0) +
-                   (IsAlive(p.Right().Down()) ? 1 : 0);
+            return (IsAlive(Left(index)) ? 1 : 0) +
+                   (IsAlive(Right(index)) ? 1 : 0) +
+                   (IsAlive(Up(index)) ? 1 : 0) +
+                   (IsAlive(Down(index)) ? 1 : 0) +
+                   (IsAlive(Up(Left(index))) ? 1 : 0) +
+                   (IsAlive(Down(Left(index))) ? 1 : 0) +
+                   (IsAlive(Up(Right(index))) ? 1 : 0) +
+                   (IsAlive(Down(Right(index))) ? 1 : 0);
         }
 
         private bool CanStop()
